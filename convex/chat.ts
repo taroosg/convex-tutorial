5910799b569841673b6749416bda2a9f0f563dfe
taroosg <root@taroosg.dev>
2025-06-09 15:55:03 +0900

    done api

--- convex/chat.ts
+++ convex/chat.ts
@@ -1,5 +1,6 @@
-import { query, mutation } from "./_generated/server";
+import { query, mutation, internalAction } from "./_generated/server";
 import { v } from "convex/values";
+import { api, internal } from "./_generated/api";
 
 export const sendMessage = mutation({
   args: {
@@ -12,6 +13,13 @@ export const sendMessage = mutation({
       user: args.user,
       body: args.body,
     });
+    if (args.body.startsWith("/wiki")) {
+      // Get the string after the first space
+      const topic = args.body.slice(args.body.indexOf(" ") + 1);
+      await ctx.scheduler.runAfter(0, internal.chat.getWikipediaSummary, {
+        topic,
+      });
+    }
   },
 });
 
@@ -24,3 +32,23 @@ export const getMessages = query({
     return messages.reverse();
   },
 });
+
+export const getWikipediaSummary = internalAction({
+  args: { topic: v.string() },
+  handler: async (ctx, args) => {
+    const response = await fetch(
+      "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=" +
+        args.topic,
+    );
+
+    const summary = getSummaryFromJSON(await response.json());
+    await ctx.scheduler.runAfter(0, api.chat.sendMessage, {
+      user: "Wikipedia",
+      body: summary,
+    });  },
+});
+
+function getSummaryFromJSON(data: any) {
+  const firstPageId = Object.keys(data.query.pages)[0];
+  return data.query.pages[firstPageId].extract;
+}
